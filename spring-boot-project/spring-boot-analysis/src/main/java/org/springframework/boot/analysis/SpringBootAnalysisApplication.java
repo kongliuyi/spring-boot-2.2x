6229b52d239e61841799942a737e915d60097e7d
kongliuyi <315294970>
2020-02-05 13:37:15 +0800

    Initial commit

---
+++ spring-boot-project/spring-boot-analysis/src/main/java/org/springframework/boot/analysis/SpringBootAnalysisApplication.java
@@ -0,0 +1,12 @@
+package org.springframework.boot.analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringBootAnalysisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAnalysisApplication.class, args);
+    }
+}
